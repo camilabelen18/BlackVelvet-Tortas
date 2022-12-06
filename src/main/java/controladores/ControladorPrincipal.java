44f69e56920812703ab354ef7eb32c6ff3ecf195
camilabelen18 <63693694+camilabelen18@users.noreply.github.com>
2022-12-06 20:30:01 -0300

    proyecto limpio

---
+++ src/main/java/controladores/ControladorPrincipal.java
@@ -0,0 +1,23 @@
+package controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class ControladorPrincipal {
+	
+
+	
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+    public ModelAndView inicio() {
+		
+
+        return new ModelAndView("index");
+    }
+	
+	
+
+}
